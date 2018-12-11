0a105d7597e09d538caba41733a3fca4b9ec44bc
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-11 15:58:21 +0000

    first test passed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-  
+        <form id='request'>
+          Title:
+          <input type='text' id='title' value='title'/>
+          <input type='body' id='body' value='body'/>
+          <input type='location' id='location' value='location'/>
+          <input type='start_date' id='start_date' value='start_date'/>
+          <input type='end_date' id='end_date' value='end_date'/>
+        </form>
         </header>
       </div>
     );

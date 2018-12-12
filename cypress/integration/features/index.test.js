f309e0b23720c8aa92276d89593667b476c7fd76
muge <muge.hasilci@hotmail.com>
2018-12-12 14:16:27 +0000

    wrote test for listings table

--- cypress/integration/features/index.test.js
+++ cypress/integration/features/index.test.js
@@ -1,36 +1,47 @@
-describe('Index', function() {
-  it('Should Render', function() {
-    cy.visit('/')
-    cy.contains('app')
-    cy.get('form').within(() => {
-      cy.get('#title').type('title')
-      cy.get('#description').type('body')
-      cy.get('#start_date').type('start_date')
-      cy.get('#end_date').type('end_date')
-      cy.get('#listing_type').type('listing_type')
-      cy.get('#location').type('location')
-      cy.contains("submit").click()
-    })
-  })
-  it('should create data object', function() {
-    cy.visit('/')
-    cy.get('#title')
-      .type('Help')
-        .should('have.value', 'Help')
-    cy.get('#description')
-      .type('Need help')
-        .should('have.value', 'Need help')
-    cy.get('#start_date')
-      .type('June')
-        .should('have.value', 'June')
-    cy.get('#end_date')
-      .type('July')
-        .should('have.value', 'July')
-    cy.get('#listing_type')
-      .type('Listing FOO')
-        .should('have.value', 'Listing FOO')
-    cy.get('#location')
-      .type('Here')
-        .should('have.value', 'Here')
-  })
-})
+describe("Index", function() {
+  it("Should Render", function() {
+    cy.visit("/");
+    cy.contains("app");
+    cy.get("form").within(() => {
+      cy.get("#title").type("title");
+      cy.get("#description").type("body");
+      cy.get("#start_date").type("start_date");
+      cy.get("#end_date").type("end_date");
+      cy.get("#listing_type").type("listing_type");
+      cy.get("#location").type("location");
+      cy.contains("submit").click();
+    });
+  });
+  it("should create data object", function() {
+    cy.visit("/");
+    cy.get("#title")
+      .type("Help")
+      .should("have.value", "Help");
+    cy.get("#description")
+      .type("Need help")
+      .should("have.value", "Need help");
+    cy.get("#start_date")
+      .type("June")
+      .should("have.value", "June");
+    cy.get("#end_date")
+      .type("July")
+      .should("have.value", "July");
+    cy.get("#listing_type")
+      .type("Listing FOO")
+      .should("have.value", "Listing FOO");
+    cy.get("#location")
+      .type("Here")
+      .should("have.value", "Here");
+  });
+  it("should list all the listings", function() {
+    cy.visit("/");
+    cy.get("listing_table").within(() => {
+      cy.contains("Help Needed");
+      cy.contains("Help Description");
+      cy.contains("June");
+      cy.contains("July");
+      cy.contains("Request");
+      cy.contains("London");
+    });
+  });
+});
--- src/FormListings.js
+++ src/FormListings.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class FormListings extends Component {
   constructor(props) {
     super(props);
 
     this.initialState = {
-      title: '',
-      description: '',
-      start_date: '',
-      end_date: '',
-      listing_type: '',
-      location: ''
+      title: "",
+      description: "",
+      start_date: "",
+      end_date: "",
+      listing_type: "",
+      location: ""
     };
 
     this.state = this.initialState;
   }
 
   handleChange = event => {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
 
-      this.setState({
-      [name] : value
+    this.setState({
+      [name]: value
     });
-  }
+  };
 
   render() {
     const {
@@ -42,47 +42,54 @@ class FormListings extends Component {
           id="title"
           name="title"
           value={title}
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+        />
         <label>Description</label>
         <input
           type="text"
           id="description"
           name="description"
           value={description}
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+        />
         <label>Start Date</label>
         <input
           type="text"
           id="start_date"
           name="start_date"
           value={start_date}
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+        />
         <label>End Date</label>
         <input
           type="text"
           id="end_date"
           name="end_date"
           value={end_date}
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+        />
         <label>Listing Type</label>
         <input
           type="text"
           id="listing_type"
           name="listing_type"
           value={listing_type}
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+        />
         <label>Location</label>
         <input
           type="text"
           id="location"
           name="location"
           value={location}
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+        />
         <input
           type="submit"
           id="submit"
           value="submit"
-          onClick={this.submitForm} />
+          onClick={this.submitForm}
+        />
       </form>
     );
   }

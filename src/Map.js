898e038997743e80263aa668c446dc44bfab0f08
Muna <mmhussein125@gmail.com>
2018-12-17 11:54:51 +0000

    Maps implementation with hardcoded data

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,59 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Map } from 'google-maps-react';
+import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
-export class Map extends Component {
+class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+    onClose = props => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        });
+      }
+    };
+
   render() {
-    return(
-      <div>
-        Map
-      </div>
-      );
-    }
+    return (
+      <Map
+       google={this.props.google}
+       zoom={14}
+       initialCenter={{
+         lat: 51.5192789,
+         lng: -0.1321629
+        }}
+         >
+
+         <Marker
+          onClick={this.onMarkerClick}
+          name={'The British Museum'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
   }
- export default Map
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_MAPS_API_KEY
+})(MapContainer);

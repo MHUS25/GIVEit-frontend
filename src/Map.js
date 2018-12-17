e2c5afe05f135d570df0d3c8adf0895f1739e49d
Muna <mmhussein125@gmail.com>
2018-12-17 13:54:33 +0000

    Rewriting Map component

--- src/Map.js
+++ src/Map.js
@@ -2,58 +2,10 @@ import React, { Component } from 'react';
 import { Map } from 'google-maps-react';
 import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
-class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {}
-  };
+class Map extends Component {
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-
-    onClose = props => {
-      if (this.state.showingInfoWindow) {
-        this.setState({
-          showingInfoWindow: false,
-          activeMarker: null
-        });
-      }
-    };
-
-  render() {
-    return (
-      <Map
-       google={this.props.google}
-       zoom={14}
-       initialCenter={{
-         lat: 51.5192789,
-         lng: -0.1321629
-        }}
-         >
-
-         <Marker
-          onClick={this.onMarkerClick}
-          name={'The British Museum'}
-        />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
 }
 
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_MAPS_API_KEY
-})(MapContainer);
+})(Map);

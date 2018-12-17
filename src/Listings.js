adfcebb0ddc8c5eb4ad3619fc68363f84a660fc0
Muna <mmhussein125@gmail.com>
2018-12-17 11:31:31 +0000

    Routing

---
+++ src/Listings.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import FormListings from "./FormListings";
+import TableListings from "./TableListings";
+import Map from "./Map";
+
+
+class App extends Component {
+  state = {
+    listings: []
+  };
+
+  componentDidMount() {
+    const url = 'https://giveit-backend.herokuapp.com/listings';
+
+    fetch(url)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          listings: result
+        })
+      });
+  }
+
+  handleSubmit = listing => {
+    this.setState({ listings: [...this.state.listings, listing] });
+    fetch('https://giveit-backend.herokuapp.com/listings', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        listing
+      }),
+    }).then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.listing;
+      })
+  };
+
+  removeList = (rowId, index) => {
+    const { listings } = this.state;
+
+    this.setState({
+      listings: listings.filter((listing, i) => {
+        return i !== index;
+      })
+    });
+    return fetch(`https://cors-anywhere.herokuapp.com/https://giveit-backend.herokuapp.com/listings/${rowId}`, {
+      method: 'DELETE',
+      headers: {
+        'Origin': 'x-requested-with'
+      },
+    });
+  };
+
+  render() {
+
+    return (
+      <div className="app">
+        <TableListings
+          listings={this.state.listings}
+          removeList={this.removeList}
+        />
+        <FormListings handleSubmit={this.handleSubmit} />
+        <Map/>
+      </div>
+    );
+  }
+}
+
+export default App;

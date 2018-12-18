5e53899600746f22e98fa05f591f24f43aeaacf9
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-18 15:15:34 +0000

    Update adding_a_listing.test with new params

--- cypress/integration/features/adding_a_listing.test.js
+++ cypress/integration/features/adding_a_listing.test.js
@@ -1,12 +1,15 @@
 describe('Creating a listing', function () {
   it ('has a form which can be filled', function() {
     cy.visit('/')
-    cy.get('#title').type('Test title')
-    cy.get('#description').type('Test description')
-    cy.get('#start_date').type('Test startdate')
-    cy.get('#end_date').type('Test enddate')
-    cy.get('#listing_type').type('Test listingtype')
+    cy.get('#title').type('I need some comic books')
+    cy.get('#description').type('Basically just really bored')
+    cy.get('#start_date').type('25/12/2018')
+    cy.get('#end_date').type('25/12/2018')
+    cy.get('#listing_type').type('Need')
     cy.get('#location').type('E1 6LT')
+    cy.get('#phone_number').type('020 7499 9000')
+    cy.get('#email').type('barack@obama.org')
+    cy.get('#user_name').type('Barack')
     cy.get('#submit').click()
   });
 });

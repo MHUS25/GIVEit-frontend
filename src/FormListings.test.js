f488009e6461e66924cef5173d68fefbfe9beead
Muna <mmhussein125@gmail.com>
2018-12-18 10:50:18 +0000

    FormListings render without crashing

---
+++ src/FormListings.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormListings from './FormListings'
+import {shallow, mount} from 'enzyme'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+describe('FormListings', () => {
+  it('should render without crashing', () => {
+    shallow(<FormListings/>);
+  });
+});

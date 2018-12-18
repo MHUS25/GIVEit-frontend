34cbb92c218a889cdf9070851e4bbcc2e56ada04
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-18 13:51:18 +0000

    messing with stuff

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 
 const NavBar = () => {
@@ -12,6 +14,13 @@ const NavBar = () => {
                 <Typography variant="title" color="inherit">
                 GIVEit
                 </Typography>
+                <TextField
+                  style={{ padding: 24 }}
+                  className="SearchBar"
+                  placeholder="Search"
+                  value="tempChangeMeL8tr"
+                  // onChange={(e) => this.setState({ searchfor:e.target.value})}
+                  />
             </Toolbar>
         </AppBar>
         </div>

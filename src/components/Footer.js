61d7085859e852bf3d4474bde4bace51683cf0c8
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-19 15:50:50 +0000

    fixed typo bug failing ci

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,14 @@ function Footer(){
         This App was created by:
         <a href="https://github.com/RyanWolfen7"> Ryan Clark, </a>
         <a href="https://github.com/MugeHasilci"> Muge Hasilci, </a>
-        <a href="hhttps://github.com/MHUS25"> Muna Hussein, </a>
+        <a href="https://github.com/MHUS25"> Muna Hussein, </a>
         <a href="https://github.com/hnryjmes"> Henry Cooksley, </a>
         <a href="https://github.com/becc-mu"> Rebecca Mulugeta </a>
       </div>
 
       <div class='resources'>
         <ul>
-          API Integration with: 
+          API Integration with:
           <a href="https://developers.google.com/maps/documentation/javascript/get-api-key">
             Google Maps
           </a>

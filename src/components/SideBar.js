239892320b6ec6e158fc150286ef7358944ddec4
muge <muge.hasilci@hotmail.com>
2018-12-19 12:23:18 +0000

    fixing `ci issue

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -46,7 +46,7 @@ class FormListings extends Component {
     } = this.state;
 
     return (
-      <Menu customBurgerIcon={ <img src="http://www.clker.com/cliparts/J/N/5/l/n/k/add-button-blue-hi.png" /> }>
+      <Menu customBurgerIcon={ <img src="http://www.clker.com/cliparts/J/N/5/l/n/k/add-button-blue-hi.png" alt="Add Post" /> }>
         <form>
           <label>Title</label>
           <input

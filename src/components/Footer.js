d1b65c0adf275696ff98e53716368720b1b11152
Muna <mmhussein125@gmail.com>
2018-12-19 17:07:35 +0000

    Adds repo link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,13 @@ import '../App.css'
 
 function Footer(){
   return (
-    <div id='footer-text'>
-      GIVEit © 2018
-    </div>
+      <div id='footer-text'>
+        GIVEit © 2018
+        <p>
+        <a href="https://github.com/MugeHasilci/GIVEit-frontend" id='footer-text2'> Code for this project can be foound in this repo</a>
+        </p>
+      </div>
+
   )
 }
 

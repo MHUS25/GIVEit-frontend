7a4354608494f90e0099ac45e60d9514a1caba60
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-20 12:39:36 +0000

    Change to list icon

--- src/components/ListingBar.js
+++ src/components/ListingBar.js
@@ -8,7 +8,7 @@ class TableListings extends Component {
 
     return (
     <div className='menu-2'>
-      <Menu width={ '900px' } right>
+      <Menu customBurgerIcon={ <img src="./list.png" alt="View Listings" /> } width={ '900px' } right>
         <table>
           <TableHeader />
           <TableBody listings={listings}
